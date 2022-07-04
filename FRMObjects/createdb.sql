82d1a5db9414aeb2e00bf6060c53bc7695c42b03
Mark Smith <smith404@users.noreply.github.com>
2022-07-04 19:25:23 +0200

    Added database script

---
+++ FRMObjects/createdb.sql
@@ -0,0 +1,26 @@
+
+CREATE TABLE [dbo].[HubPage] (
+    [Id]       BIGINT         IDENTITY (1000, 1) NOT NULL,
+    [Title]    NVARCHAR (255) NOT NULL,
+    [Headline] NVARCHAR (255) NOT NULL,
+    [Summary]  NVARCHAR (MAX) NOT NULL,
+    [Image]    NVARCHAR (255) NOT NULL,
+    [Body]     NVARCHAR (MAX) NOT NULL,
+    [URL]      NVARCHAR (255) NULL,
+    CONSTRAINT [PK_HubPage] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+CREATE TABLE [dbo].[HubPageSection] (
+    [Id]        BIGINT         IDENTITY (1000, 1) NOT NULL,
+    [HubPageId] BIGINT         NOT NULL,
+    [Title]     NVARCHAR (255) NOT NULL,
+    [Headline]  NVARCHAR (255) NOT NULL,
+    [Summary]   NVARCHAR (MAX) NOT NULL,
+    [Image]     NVARCHAR (255) NOT NULL,
+    [Body]      NVARCHAR (MAX) NOT NULL,
+    [URL]       NVARCHAR (255) NOT NULL,
+    CONSTRAINT [PK_HubPageSection] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_HubPage] FOREIGN KEY ([HubPageId]) REFERENCES [dbo].[HubPage] ([Id])
+);
+
+
