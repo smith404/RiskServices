8b874c40ba2052303d6b21828385f9fa17eebb40
markg <markgrahamsmith6@gmail.com>
2022-07-26 21:27:09 +0200

    Lots of changes

---
+++ FRMDesktop/Controllers/NotificationController.cs
@@ -0,0 +1,77 @@
+﻿using FRMObjects;
+using FRMObjects.model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FRMDesktop.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NotificationController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public NotificationController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("GetNotifiationCount")]
+        public int GetNotifiationCount(string user, bool processed = false)
+        {
+            using (FRP_LandingContext context = new())
+            {
+                int? count = context.Notifications?.Count(item => item.Owner == user && item.Processed == processed);
+
+                if (count == null)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return (int)count;
+                }
+            }
+        }
+
+        [HttpGet("GetNotifiations")]
+        public List<Notification> GetNotifiations(string user, bool processed = false)
+        {
+            try
+            {
+                using (FRP_LandingContext context = new())
+                {
+                    return context.Notifications.Where(item => item.Owner == user && item.Processed == processed).ToList();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+            }
+
+            return new List<Notification>();
+        }
+
+        [HttpPut("ProcessNotifiation")]
+        public List<Notification> ProcessNotifiation(Notification item)
+        {
+            try
+            {
+                using (FRP_LandingContext context = new())
+                {
+                    item.Processed = true;
+                    item.ProcessedOn = DateTime.Now;
+                    context.Notifications.Update(item);
+                    int count = context.SaveChanges();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+            }
+
+            return new List<Notification>();
+        }
+    }
+}
--- FRMObjects/createdb.sql
+++ FRMObjects/createdb.sql
@@ -54,7 +54,7 @@ CREATE TABLE [dbo].[ToolDefinition] (
 );
 GO
 CREATE TABLE [dbo].[ToolStep] (
-    [Id]          BIGINT         IDENTITY (1, 1) NOT NULL,
+    [Id]          BIGINT         IDENTITY (1000, 1) NOT NULL,
     [Description] NVARCHAR (255) NOT NULL,
     [StepType]    CHAR (3)       NOT NULL,
     [Definition]  NVARCHAR (MAX) NOT NULL,
@@ -114,3 +114,139 @@ CREATE TABLE [dbo].[Notifications] (
     CONSTRAINT [PK_Notifications] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 GO
+CREATE TABLE [dbo].[Profile] (
+    [Id]          BIGINT         IDENTITY (1000, 1) NOT NULL,
+    [UserName]    NVARCHAR (255) NOT NULL,
+    [ProfileName] NVARCHAR (255) NOT NULL,
+    [ChannelId]   BIGINT         NOT NULL,
+    [Type]        NVARCHAR (50)  NOT NULL,
+    [Icon]        NVARCHAR (50)  NOT NULL,
+    [Message]     NVARCHAR (255) NOT NULL,
+    CONSTRAINT [PK_Profile] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [Index_UserName]
+    ON [dbo].[Profile]([UserName] ASC);
+GO
+CREATE TABLE [dbo].[Channel] (
+    [Id]          BIGINT         IDENTITY (1000, 1) NOT NULL,
+    [ChannelName] NVARCHAR (255) NOT NULL,
+    [Type]        NVARCHAR (50)  NOT NULL,
+    [Icon]        NVARCHAR (50)  NOT NULL,
+    [Message]     NVARCHAR (255) NOT NULL,
+    [Private]     BIT            NOT NULL,
+    [Owner]       BIGINT         NOT NULL,
+    CONSTRAINT [PK_Channel] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [Index_ChannelName]
+    ON [dbo].[Channel]([ChannelName] ASC);
+GO
+CREATE TABLE [dbo].[ProfileChannel] (
+    [ProfileId] BIGINT   NOT NULL,
+    [ChannelId] BIGINT   NOT NULL,
+    [Status]    CHAR (1) NOT NULL,
+    CONSTRAINT [PK_ProfileChannel] PRIMARY KEY CLUSTERED ([ProfileId] ASC, [ChannelId] ASC),
+);
+GO
+CREATE TABLE [dbo].[NotificationChannel] (
+    [NotificationId] BIGINT   NOT NULL,
+    [ChannelId]      BIGINT   NOT NULL,
+    [Status]         CHAR (1) NOT NULL,
+    CONSTRAINT [PK_NotificationChannel] PRIMARY KEY CLUSTERED ([NotificationId] ASC, [ChannelId] ASC),
+);
+GO
+CREATE VIEW [dbo].[V_ProcessedNotifications]
+AS
+SELECT n.*, p.UserName, c.ChannelName, s.Status AS ProcessedStatus, s.ProcessedOn
+FROM dbo.Notifications n, dbo.Channel c, dbo.Profile p, dbo.ProfileChannel j, NotificationStatus s
+WHERE n.ChannelId = c.Id
+AND j.ProfileId = p.Id
+AND j.ChannelId = c.Id
+AND s.NotificationId = n.Id
+AND s.ProfileId = p.Id
+GO
+CREATE VIEW [dbo].[V_UnprocessedNotifications]
+AS
+SELECT n.*, p.UserName, c.ChannelName
+FROM dbo.Notifications n, dbo.Channel c, dbo.Profile p, dbo.ProfileChannel j
+WHERE n.ChannelId = c.Id
+AND j.ProfileId = p.Id
+AND j.ChannelId = c.Id
+AND NOT EXISTS (SELECT 1 FROM NotificationStatus s WHERE s.NotificationId = n.Id AND s.ProfileId = p.Id)
+GO
+CREATE PROCEDURE [dbo].[GetNotifications]	@UserName nvarchar(255)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+	IF EXISTS (SELECT 1 FROM dbo.Profile WHERE UserName = @UserName)
+	BEGIN
+		SELECT Id, Type, Icon, Message, URL, Sender, ChannelId, Status, CreatedOn, UserName, ChannelName
+		FROM dbo.V_UnprocessedNotifications
+		WHERE UserName = @UserName
+	END
+	ELSE
+	BEGIN
+		BEGIN TRANSACTION
+		DECLARE @ProfileId BIGINT;
+		DECLARE @ChannelId BIGINT;
+		INSERT INTO dbo.Profile(UserName,DisplayName,Type,Icon,Message,ChannelId)
+		VALUES(@UserName,@UserName,'User','None','Auto Generated Account',0)
+		SET @ProfileId = @@IDENTITY
+		INSERT INTO dbo.Channel(ChannelName,Type,Icon,Message,Private,Owner)
+		VALUES(@UserName,'Profile','None','Auto Generated Channel',1,@ProfileId)
+		SET @ChannelId = @@IDENTITY
+		UPDATE dbo.Profile SET ChannelId = @ChannelId
+		WHERE Id = @ProfileId
+		INSERT INTO dbo.ProfileChannel(ProfileId,ChannelId,Status)
+		VALUES(@ProfileId,@ChannelId,'A')
+		COMMIT TRANSACTION
+		SELECT Id, Type, Icon, Message, URL, Sender, ChannelId, Status, CreatedOn, UserName, ChannelName
+		FROM dbo.V_UnprocessedNotifications
+		WHERE UserName = @UserName
+	END
+END
+GO
+CREATE PROCEDURE [dbo].[SendNotification]	@Sender nvarchar(255), @ChannelName nvarchar(255), @Type  nvarchar(50), @Icon nvarchar(50), @Message nvarchar(MAX), @URL nvarchar(MAX)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+	IF EXISTS (SELECT 1 FROM dbo.Channel WHERE ChannelName = @ChannelName)
+	BEGIN
+		DECLARE @ChannelId BIGINT;
+		SELECT @ChannelId = Id
+		FROM dbo.Channel
+		WHERE ChannelName = @ChannelName
+		INSERT INTO dbo.Notifications(Type,Icon,Message,URL,Sender,ChannelId,Status,CreatedOn)
+		VALUES(@Type,@Icon,@Message,@URL,@Sender,@ChannelId,'N',CURRENT_TIMESTAMP)
+	END
+END
+GO
+CREATE TABLE [dbo].[AttributeMetadata] (
+    [id]                BIGINT        NOT NULL,
+    [id_entity]         BIGINT        NOT NULL,
+    [name]              VARCHAR (50)  NOT NULL,
+    [description]       VARCHAR (250) NOT NULL,
+    [depricated]        BIT           NOT NULL,
+    [id_attribute_type] BIGINT        NOT NULL,
+    CONSTRAINT [PK_AttributeMetadata] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+GO
+CREATE TABLE [dbo].[AttributeType] (
+    [id]        BIGINT       NOT NULL,
+    [name]      VARCHAR (50) NOT NULL,
+    [type_name] VARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_AttributeType] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+GO
+CREATE TABLE [dbo].[EntityMetadata] (
+    [id]          BIGINT        NOT NULL,
+    [name]        VARCHAR (50)  NOT NULL,
+    [description] VARCHAR (250) NOT NULL,
+    [depricated]  BIT           NOT NULL,
+    [table_name]  VARCHAR (50)  NOT NULL,
+    [id_key]      BIGINT        NOT NULL,
+    CONSTRAINT [PK_EntityMetadata] PRIMARY KEY CLUSTERED ([id] ASC)
+);
